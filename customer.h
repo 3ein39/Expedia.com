8feebf0396fccc893b91da8ee0854c548586adc3
HusseinHany <hhany3997@gmail.com>
2022-08-01 20:56:11 +0200

    Create customer header

---
+++ customer.h
@@ -0,0 +1,58 @@
+//
+// Created by hussein on 8/1/22.
+//
+
+#ifndef EXPEDIA_COM_CUSTOMER_H
+#define EXPEDIA_COM_CUSTOMER_H
+
+#include <sstream>
+using namespace std;
+
+#include "expedia_payment_card.h"
+#include "expedia_common_reservation.h"
+#include "expedia_user.h"
+
+class Customer: public User {
+    protected:
+    vector<PaymentCard*> cards;   // * to allow polymorphism
+    ReservationsSet reservations;
+
+    public:
+    // prevent copy constructor and assignment operator
+    Customer(const Customer&) = delete;
+    void operator=(const Customer&) = delete;
+
+    // Deafult constructor
+    Customer() {
+        SetType(UserType::CusomterUser);
+    }
+
+    // Destructor
+    ~Customer() {
+        for (PaymentCard* card : cards)
+            delete card;
+        cards.clear();
+    }
+
+    // Add a payment card to the customer
+    void AddPaymentCard(const PaymentCard& card) {
+        cards.push_back(card.Clone());
+    }
+
+    // Add a reservation to the customer
+    void AddReservation(const Reservation& reservation) {
+        reservations.AddReservation(*reservation.Clone());
+    }
+
+    // Get the customer's cards
+    const vector<PaymentCard*>& GetCards() const {
+        return cards;
+    }
+
+    // Get the customer's reservations
+    const ReservationsSet& GetReservations() const {
+        return reservations;
+    }
+};
+
+#endif //EXPEDIA_COM_CUSTOMER_H
