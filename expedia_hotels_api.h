6e017d95c6e154888b4da98952639baa16278057
HusseinHany <hhany3997@gmail.com>
2022-08-02 00:52:18 +0200

    write HotelRoom class

--- expedia_hotels_api.h
+++ expedia_hotels_api.h
@@ -8,6 +8,12 @@
 #ifndef EXPEDIA_HOTELS_API_H_
 #define EXPEDIA_HOTELS_API_H_
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include "common_reservation.h"
+
 class HiltonRoom {
 public:
 	string room_type;
@@ -51,5 +57,94 @@ public:
 		return rooms;
 	}
 };
+
+class HotelRoom {
+private:
+    string hotel_name;
+    string room_type;
+    int available_rooms = 0;
+    double price_per_night = 0;
+    string date_from;
+    string date_to;
+
+public:
+    // printing
+    void ToString() const {
+        ostringstream oss;
+        oss << "Hotel: " << hotel_name << " Room Type: " << room_type << "(" << available_rooms << ") Price per night: " << price_per_night << " From Date " << date_from << " to " << date_to << "";
+        return oss.str();
+    }
+
+    // setters and getters
+    const string& GetDateFrom() const {
+        return date_from;
+    }
+
+    void SetDateFrom(const string& date_from) {
+        this->date_from = date_from;
+    }
+
+    const string& GetDateTo() const {
+        return date_to;
+    }
+
+    void SetDateTo(const string& date_to) {
+        this->date_to = date_to;
+    }
+
+    double GetPricePerNight() const {
+        return price_per_night;
+    }
+
+    void SetPricePerNight(double price_per_night) {
+        this->price_per_night = price_per_night;
+    }
+
+    const string& GetRoomType() const {
+        return room_type;
+    }
+
+    void SetRoomType(const string& room_type) {
+        this->room_type = room_type;
+    }
+
+    int GetAvailableRooms() const {
+        return available_rooms;
+    }
+
+    void SetAvailableRooms(int available_rooms) {
+        this->available_rooms = available_rooms;
+    }
+
+    const string& GetHotelName() const {
+        return hotel_name;
+    }
+
+    void SetHotelName(const string& hotel_name) {
+        this->hotel_name = hotel_name;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif /* EXPEDIA_HOTELS_API_H_ */
